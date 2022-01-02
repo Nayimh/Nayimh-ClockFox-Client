cc69977f3ec99e4c871d302a0dc33d5ed20f8375
fahim-42 <you@example.com>
2022-01-02 21:05:23 +0600

    url changed

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -14,8 +14,7 @@ const Reviews = () => {
 
     const [review, setReview] = useState([]);
     useEffect(() => {
-        // const url = 'http://localhost:3030/reviews';
-        const url = 'https://hidden-hamlet-39500.herokuapp.com/reviews';
+        const url = 'http://localhost:3030/reviews';
         fetch(url)
             .then(res => res.json())
             .then(data => setReview(data));
